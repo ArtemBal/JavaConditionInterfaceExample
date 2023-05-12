5542ce5317c8209007cba9da358c947ca7c3fdde
Artem Balan <a.s.balan96@gmail.com>
2023-05-12 22:30:07 +0300

    Condition realization

--- src/MessageQueue.java
+++ src/MessageQueue.java
@@ -1,2 +1,34 @@
-public class MessageQueue {
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class MessageQueue<T> {
+    private Queue<T> queue = new LinkedList<>();
+    private Lock lock = new ReentrantLock();
+    private Condition hasMessages = lock.newCondition();
+
+    public void publish(T message) {
+        lock.lock();
+        try {
+            queue.offer(message);
+            hasMessages.signal();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public T receive() throws InterruptedException {
+        lock.lock();
+        try {
+            while (queue.isEmpty()) {
+                hasMessages.await();
+            }
+            return queue.poll();
+        } finally {
+            lock.unlock();
+        }
+    }
+
 }
